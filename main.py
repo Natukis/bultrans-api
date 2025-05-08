50f9e24295f10868d784bd0ff0f2a82f02514c73
Natukis <108143334+Natukis@users.noreply.github.com>
2025-05-08 22:08:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-from fastapi import FastAPI, UploadFile, Form
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.responses import JSONResponse
 from process import process_invoice_upload
 
 app = FastAPI()
 
-# אפשר להתאים דומיין/מקור מאובטח
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # עדיף להגביל למקור מסוים בפרודקשן
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.post("/process-invoice")
-async def process_invoice(
+@app.post("/process-invoice/")
+async def generate_invoice(
     client_id: int = Form(...),
-    file: UploadFile = Form(...),
-    template: UploadFile = Form(...)
+    file: UploadFile = File(...),
+    template: UploadFile = File(...)
 ):
     return await process_invoice_upload(client_id, file, template)

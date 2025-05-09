c5c8e8473f323455ee122e6f3626ea4a0e2cc5a3
Natukis <108143334+Natukis@users.noreply.github.com>
2025-05-09 21:17:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI, UploadFile, File, Form
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi import FastAPI, UploadFile, Form
+from fastapi.responses import FileResponse, JSONResponse
 from process import process_invoice_upload
 import os
 
 app = FastAPI()
 
 @app.post("/process-invoice/")
-async def generate_invoice(
-    supplier_id: int = Form(...),
-    file: UploadFile = File(...),
-    template: UploadFile = File(...)
+async def process_invoice(
+    supplier_id: str = Form(...),
+    file: UploadFile = Form(...),
+    template: UploadFile = Form(...)
 ):
     return await process_invoice_upload(supplier_id, file, template)
 
@@ -17,5 +17,5 @@ async def generate_invoice(
 def download_invoice(filename: str):
     file_path = f"/tmp/{filename}"
     if os.path.exists(file_path):
-        return FileResponse(path=file_path, filename=filename, media_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-    return JSONResponse(content={"error": "File not found"}, status_code=404)
+        return FileResponse(path=file_path, filename=filename)
+    return JSONResponse({"error": "File not found"}, status_code=404)
